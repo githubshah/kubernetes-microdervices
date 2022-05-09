e4ad95425bd340eff0acd5e27bbd790f4e39c79b
githubshah <72010111+githubshah@users.noreply.github.com>
2022-05-09 08:51:47 +0530

    done

---
+++ README.md
@@ -0,0 +1,56 @@
+
+// MUST use SNAPSHOT whilke creating iimage
+
+// create exchange image and conversion image.
+
+docker build -t shaid/ce3:0.0.121-SNAPSHOT .                                        (create image)
+docker image ls                                                                     (check imgae created or not)
+
+shaid/ce3:0.0.121-SNAPSHOT                                                           (image name looks like this)
+
+// creat deployment for both above images
+
+kubectl create deployment ce3 --image=shaid/ce3:0.0.121-SNAPSHOT                               (create deployment)
+ 
+kubectl expose deployment ce3 --type=LoadBalancer --port=8000                                  (expose deployment)
+
+// check external_ip should be not null and ready shuold be not zero
+
+kubectl get all                                                                      
+
+#kubectl get events --logs pod/ce2-5955df974c-tvt8r
+
+#kubectl scale deployment ce1 --replicas=3
+
+#kubectl rollout restart deployment/ce2
+
+kubectl get deployment cc3 -o yaml                                                         (Check deployment configuration)
+kubectl get service cc3_service -o yaml
+
+kubectl get deployment cc3 -o yaml > cc3.yaml                                          (deployment config output to a file)
+kubectl get service cc3 -o yaml > cc3_service.yaml                                        (service config output to a file) 
+
+
+// always create new ymal file for deployment update to avoid conflicts 
+// use env properties from cc1.yaml file to new cc3.ymal file you create above 
+// optional merge service yaml file into deployment yaml file and saparated with three hyphon ---
+
+kubectl diff -f cc3.yaml                                                                      (diff in ymal exising vs new)
+kubectl apply -f cc3.yaml   (only this cmd will create deployment)                            (apply ymal changes in deployment)
+
+#kubectl delete all -l app=<service name>
+
+kubectl logs -f pod/cc3-76bc97985b-gvchp                                                            (view pod logs)
+
+kubectl create configmap cc3 --from-literal=CURRENCY_EXCHANGE_SERVICE_HOST=http://cc3               (create common env config)
+
+kubectl describe deployment                                                                         (view deployment ymal config)
+
+kubectl exec cc3-7b49f4ccc-sxdgs -- printenv                                (view all deployment ENV)
+
+
+
+
+
+http://localhost:8100/currency-conversion-feign/from/USD/to/INR/quantity/10
+http://localhost:8000/currency-exchange/from/USD/to/INR
---
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,84 @@
+package com.in28minutes.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CurrencyConversionController {
+
+    @Autowired
+    private Environment environment;
+
+    private Logger logger = LoggerFactory.getLogger(CurrencyConversionController.class);
+
+    @Autowired
+    private CurrencyExchangeProxy proxy;
+
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity
+    ) {
+
+        //CHANGE-KUBERNETES
+        logger.info("calculateCurrencyConversion called with {} to {} with {}", from, to, quantity);
+
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+
+        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity
+                ("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                        CurrencyConversion.class, uriVariables);
+
+        CurrencyConversion currencyConversion = responseEntity.getBody();
+
+        return new CurrencyConversion(currencyConversion.getId(),
+                from, to, quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment() + " " + "rest template");
+
+    }
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity
+    ) {
+
+        //CHANGE-KUBERNETES
+        logger.info("calculateCurrencyConversionFeign called with {} to {} with {}", from, to, quantity);
+
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+
+        String port = environment.getProperty("local.server.port");
+
+        //CHANGE-KUBERNETES
+        String host = environment.getProperty("HOSTNAME");
+        String version = "v11";
+
+        String s = String.format("currency-conversion post:%s, version:%s, host:%s", port, version, host);
+
+        return new CurrencyConversion(currencyConversion.getId(),
+                from, to, quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment() + " " + "feign |||||| " + s);
+
+    }
+
+
+}
